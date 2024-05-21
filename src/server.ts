00111d453277ffce39565c06f79b640147964912
Richard Freitas <wesleyrichard30@gmail.com>
2024-05-21 11:45:07 -0300

    feat: add express http server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log('Hello, welcome to the server!')
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server running in port 3000');
+});

4f93924ba975dfb5dc111dfa2fb0512cbd7cd478
Richard Freitas <wesleyrichard30@gmail.com>
2024-05-22 09:39:21 -0300

    chore: add husky and lint staged

--- src/lib/sequelize.ts
+++ src/lib/sequelize.ts
@@ -2,6 +2,5 @@ import { Sequelize } from 'sequelize';
 import { env } from '../env';
 
 export const sequelize = new Sequelize(env.DATABASE_URL, {
-  dialect: 'postgres',
   logging: env.NODE_ENV === 'dev' ? console.log : false,
 });
